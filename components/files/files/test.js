c9cde40a9e8c2cf5101155245b8866283e92a5b0
santobuonoivan <mecaluxivansantobuono@gmail.com>
2019-10-09 18:08:44 -0300

    refactor sync buffers

--- components/files/files/test.js
+++ components/files/files/test.js
@@ -5,60 +5,42 @@ const _ = require('lodash');
 const eachLine = Promise.promisify(lineReader.eachLine);
 const fs = require('fs');
 
+function read_fragment(init, last) {
+    let metrics = [];
+    let response;
+    let dir = __dirname + path.sep +'file1.tsv';
+    try {
+        let fd = fs.openSync(dir, 'r');
+        let stats = fs.fstatSync(fd);
 
-let dir = __dirname + path.sep +'file1.tsv';
-try {
-    fs.open(dir, 'r', function(err, fd) {
+        let sockets = stats.size/10000;
+        let bufferSize= /*(sockets-200)*/5*1024,
+            chunkSize=1024,
+            buffer=new Buffer.alloc(bufferSize),
+            bytesRead = init;
 
-        fs.fstat(fd, function(err, stats) {
-            let sockets = stats.size;
-            let bufferSize= sockets,
-                chunkSize=1024,
-                buffer=new Buffer.alloc(bufferSize),
-                bytesRead = 0;
-
-            while (bytesRead < bufferSize) {
-                if ((bytesRead + chunkSize) > bufferSize) {
-                    chunkSize = (bufferSize - bytesRead);
-                }
-                fs.read(fd, buffer, bytesRead, chunkSize, bytesRead);
-                bytesRead += chunkSize;
+        while (bytesRead < bufferSize) {
+            if ((bytesRead + chunkSize) > bufferSize) {
+                chunkSize = (bufferSize - bytesRead);
             }
-            console.log(buffer.toString('utf8', 0, bufferSize));
-            fs.closeSync(fd);
-            //return buffer;
-        });
-    });
-        /*
-        eachLine(dir, function (line) {
-            let lineSplit = _.split(line, '\t');
-            let user_id = lineSplit[0];
-            let cod = lineSplit[2];
-            //console.log(cod);
-            let segments = _.split(lineSplit[1], ',');
-            for (let i = 0; i < segments.length; i++) {
-                const segment = segments[i];
-                metrics[segment][cod] += 1;
-
-            }
-        }).then(function (err) {
-            if (err) throw err;
-            console.log("I'm done!!");
-        }).catch(e => {
-            console.log(e.message)
-        });*/
-}catch (e) {
-    console.log(e.message)
+            fs.read(fd, buffer, bytesRead, chunkSize, bytesRead);
+            bytesRead += chunkSize;
+        }
+        //console.log(buffer.toString('utf8', 0, bufferSize));
+        fs.closeSync(fd);
+        return buffer;
+    }catch (e) {
+        console.log(e.message)
+    }
 }
 
+let buffer1 = read_fragment(0,0);
+let buffer2 = read_fragment(5120,0);
 
-/*let buffer1 = */
-//let buffer2 = read_fragment(2280,0);
 
-/*
-let bufferTotal = buffer1.lenth + buffer2.lenth;
-let totalBuffer = Buffer.concat( [buffer1,buffer2], bufferTotal);
-console.log(buffer.toString('utf8', 0, totalBuffer));
+let bufferTotalLength = buffer1.length + buffer2.length;
+let totalBuffer = Buffer.concat( [buffer1,buffer2], bufferTotalLength);
+console.log(totalBuffer.toString('utf8', 0, totalBuffer.length));
 /*
 module.exports = {
     get_metrics

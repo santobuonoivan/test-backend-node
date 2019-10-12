4872a548e8029540ee1e2e650e1ef6b772895f63
santobuonoivan <mecaluxivansantobuono@gmail.com>
2019-10-12 12:01:20 -0300

    working refactor

--- components/files/repository/workersPool.js
+++ components/files/repository/workersPool.js
@@ -3,22 +3,7 @@ const os = require('os');
 const worker = require('worker');
 const cpus = os.cpus().length;
 const moment = require('moment');
-
-/******************************************************************/
-const { MessageChannel } = require('worker_threads');
-/*
-parentPort.once("message", (param) => {
-    // return the result to main thread.
-    parentPort.postMessage(param);
-});
-*/
-
-
-
-//const { port1, port2} = new MessageChannel();
-
 const { processing, failed, started, ready } = require('./portsMensages');
-//port1.on('message', (message) => console.log('received', message));
 const path = require('path');
 
 
@@ -26,19 +11,12 @@ exports.readAndGetMetrics = async function readAndGetMetrics(res,filename) {
     // load a few hundred thousand words into an array
     let dir = __dirname + path.sep + '../files/tsv' + path.sep + filename + '.tsv';
     let rowsList = fs.readFileSync(dir, 'utf8').split('\n');
-
-
-    /***********************************************************************/
     let start = moment().format('YYYY-MM-DDTHH:MM:SS');
-
-    //port2.postMessage(started(start));
-    res.write(JSON.stringify(started(start)));
-
+    console.log(started(start));
     // create a group of workers (size defaults to os.cpus().length)
     let k_group = worker.group(__dirname + path.sep + 'myWorker.js', cpus);
     // processing pipeline
-    res.write(processing(start));
-
+    console.log(processing(start));
     /* threads init */
     k_group
     // bind data from our list, dividing array evenly among workers
@@ -54,8 +32,7 @@ exports.readAndGetMetrics = async function readAndGetMetrics(res,filename) {
         })*/
         // reduce multiple results into a single one
         .reduce('merge').then((a_merged_metrics) => {        // stop the timer
-        res.write(ready(start, a_merged_metrics));
-        res.end();
+            return res.send(ready(start, a_merged_metrics));
         //console.log(a_merged_metrics);
     });
 
